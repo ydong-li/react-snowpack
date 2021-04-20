24628d05c97931779d20b5e3a6eb723f4f38cc4d
liyingdong.liyd <liyingdong.liyd@bytedance.com>
2021-04-20 16:34:06 +0800

    react 项目初始化

---
+++ src/components/App.jsx
@@ -0,0 +1,27 @@
+import React, {useState, useEffect} from 'react';
+
+export function App() {
+  // Create the count state.
+  const [count, setCount] = useState(0);
+  // Update the count (+1 every second).
+  useEffect(() => {
+    if(count >= 20) {
+      return
+    }
+    const timer = setTimeout(() => {
+      console.log({timer});
+      setCount(count + 1)
+    }, 1000);
+    return () => clearTimeout(timer);
+  }, [count, setCount]);
+  // Return the App component.
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Page has been open for <code>{count}</code> seconds.
+        </p>
+      </header>
+    </div>
+  );
+}
